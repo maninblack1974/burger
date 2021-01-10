d026e2d00aa76997de2122f45f741313381104f4
DRH <25594179+maninblack1974@users.noreply.github.com>
2021-01-09 23:04:32 -0800

    Adding the file structure and the db schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Double');
+INSERT INTO burgers (name, devoured) VALUES ('4x4', true);
+INSERT INTO burgers (name, devoured) VALUES ('8x8', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Double Animal Style', true);
+INSERT INTO burgers (name) VALUES ('Flying Dutchmen');
