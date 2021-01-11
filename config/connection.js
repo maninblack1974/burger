274e544b93e662d8a422d48655e7c78db50f265c
DRH <25594179+maninblack1974@users.noreply.github.com>
2021-01-11 01:02:45 -0800

    Change connection info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Kiddos0215!",
-  database: "burgers_db"
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection= mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "keyrdu8lxssdthgq",
+    password: "athwxrws9kl2lyzv",
+    database: "o6kpjeev9xdtkzf3"
+  });
 });
 
 connection.connect(function(err) {
